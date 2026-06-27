eee806ab141e84e4f4f2f886bb00360a66111d6c
Ronaldo93 <51114190+Ronaldo93@users.noreply.github.com>
2026-06-27 22:48:17 +0700

    feat: wrangler deployment for web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,18 +6,19 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools({
-      consolePiping: {
-        enabled: false,
-      },
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [devtools({
+    consolePiping: {
+      enabled: false,
+    },
+  }), tailwindcss(), tanstackStart(), viteReact(), cloudflare({
+    viteEnvironment: {
+      name: "ssr"
+    }
+  })],
   server: {
     proxy: {
       "^/api/v1/static": {
