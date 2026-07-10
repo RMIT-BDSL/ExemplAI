232dbbe79b95ead5b1470b7a7782a9732261b7fe
Ronaldo93 <51114190+Ronaldo93@users.noreply.github.com>
2026-07-10 23:13:43 +0700

    feat: enable full tracing for Sentry in main server configuration

--- server/main.py
+++ server/main.py
@@ -30,6 +30,7 @@ sentry_sdk.init(
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
+    traces_sample_rate=1.0, # sample every trace, for now - recommends to tune this down in production
 )
 
 log = logging.getLogger("rich")
